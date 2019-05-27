71a0ac773c2aead3f33ef1f59bdc39d028d58f64
jung <jungha0902@naver.com>
2019-05-27 23:40:05 +0900

    최대값, 최소값 구하기

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-javac -d ./bin  ./src/day01/p01/ArrayEx01.java
+javac -d ./bin  ./src/day01/p02/ArrayEx02.java
 javac -d ./bin -cp ./src ./src/day01/Start.java
 
 
-java -cp ./bin day01.Start
+java -cp ./bin day01.Start "p02"
--- src/day01/Start.java
+++ src/day01/Start.java
@@ -1,9 +1,25 @@
 package day01;
 
 import day01.p01.ArrayEx01;
+import day01.p02.ArrayEx02;
 
 public class Start{
 	public static void main(String[] args){
-		ArrayEx01.process();	
-	}		
-}
+		if(args.length == 0){
+			System.out.println("문제 번호를 입력하세요");	
+		}else{
+			switch(args[0]) {
+				case "p01" : 
+					ArrayEx01.process();
+					break;				
+				case "p02" : 
+					ArrayEx02.process();
+					break;
+				default : 
+					System.out.println("문제 번호를 확인하세요");
+					break;
+			}
+		}
+	}
+}		
+
