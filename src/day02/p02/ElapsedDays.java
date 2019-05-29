865c1fee3fd68341d614ebf733bb1b6fee1d528e
jung <jungha0902@naver.com>
2019-05-30 01:13:29 +0900

    day02.p02. 그 해의 경과일 계산하기 (소스 작성 중,,,)

---
+++ src/day02/p02/ElapsedDays.java
@@ -0,0 +1,11 @@
+package day02.p02;
+
+public class ElapsedDays {
+		public static void process(){
+
+				System.out.println("경과일을 구할 날짜를 입력 하세요");
+				Scanner sc = new Scanner(System.in);
+			
+
+		}
+}
